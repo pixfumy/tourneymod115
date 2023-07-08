1acfc9c91cb9a39029b042dd73dacc5b10390002
pixfumy <95588510+pixfumy@users.noreply.github.com>
2023-07-08 03:02:34 -0400

    [v1.1] add Custom splash

---
+++ src/main/java/net/pixfumy/tourneymod115/mixin/SplashTextMixin.java
@@ -0,0 +1,21 @@
+package net.pixfumy.tourneymod115.mixin;
+
+import net.minecraft.client.gui.screen.TitleScreen;
+import net.minecraft.client.resource.SplashTextResourceSupplier;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Overwrite;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Locale;
+
+@Mixin(SplashTextResourceSupplier.class)
+public class SplashTextMixin {
+    @Inject(method = "get", at = @At("HEAD"), cancellable = true)
+    private void getSplash(CallbackInfoReturnable<String> cir) {
+        cir.setReturnValue("Tourney Mod 1.15");
+    }
+}
