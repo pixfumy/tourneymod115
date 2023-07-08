2993a3e65c1cf5715dd1416854dd40e6c7fada7b
pixfumy <95588510+pixfumy@users.noreply.github.com>
2023-07-08 01:43:36 -0400

    [v1.1] actually standardize flint, was I stupid?

--- src/main/java/net/pixfumy/tourneymod115/mixin/FlintMixin.java
+++ src/main/java/net/pixfumy/tourneymod115/mixin/FlintMixin.java
@@ -2,9 +2,11 @@ package net.pixfumy.tourneymod115.mixin;
 
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
+import net.minecraft.block.Blocks;
 import net.minecraft.block.entity.BlockEntity;
 import net.minecraft.entity.Entity;
 import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
 import net.minecraft.loot.context.LootContext;
 import net.minecraft.loot.context.LootContextParameters;
 import net.minecraft.server.world.ServerWorld;
@@ -26,8 +28,10 @@ public class FlintMixin {
     @Inject(method = "getDroppedStacks(Lnet/minecraft/block/BlockState;Lnet/minecraft/server/world/ServerWorld;Lnet/minecraft/util/math/BlockPos;Lnet/minecraft/block/entity/BlockEntity;Lnet/minecraft/entity/Entity;Lnet/minecraft/item/ItemStack;)Ljava/util/List;",
     at = @At("HEAD"), cancellable = true)
     private static void dropFlint(BlockState state, ServerWorld world, BlockPos pos, @Nullable BlockEntity blockEntity, @Nullable Entity entity, ItemStack stack, CallbackInfoReturnable<List<ItemStack>> cir) {
-        RNGStreamGenerator rngStreamGenerator = ((ILevelProperties)world.getServer().getWorld(DimensionType.OVERWORLD).getLevelProperties()).getRNGStreamGenerator();
-        LootContext.Builder builder = (new LootContext.Builder(world)).setRandom(new Random(rngStreamGenerator.getAndUpdateSeed("flintSeed"))).put(LootContextParameters.POSITION, pos).put(LootContextParameters.TOOL, stack);
-        cir.setReturnValue(state.getDroppedStacks(builder));
+        if (state.getBlock() == Blocks.GRAVEL) {
+            RNGStreamGenerator rngStreamGenerator = ((ILevelProperties) world.getServer().getWorld(DimensionType.OVERWORLD).getLevelProperties()).getRNGStreamGenerator();
+            LootContext.Builder builder = (new LootContext.Builder(world)).setRandom(new Random(rngStreamGenerator.getAndUpdateSeed("flintSeed"))).put(LootContextParameters.POSITION, pos).put(LootContextParameters.TOOL, stack);
+            cir.setReturnValue(state.getDroppedStacks(builder));
+        }
     }
 }
